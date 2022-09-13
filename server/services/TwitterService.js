ff669ffedd27823a89d7383f9772e96f46755fb9
paul-achard <paulchardlyon@gmail.com>
2022-09-13 09:05:24 +0200

    Ajout du nettoyage du tweet (@ et url)

--- server/services/TwitterService.js
+++ server/services/TwitterService.js
@@ -3,13 +3,24 @@ import {TwitterApi} from 'twitter-api-v2';
 export default class TwitterService {
     twitterClient = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAEVJgwEAAAAA2qkI9qgA7drsV287J0FmkqtLp4M%3Dn402PjNIoMfHN0jANgPL2D8RGkiiRcvQ9TzNcj015S11kX2S3m');
 
+    cleanTweet(rawTweet) {
+        // remove @exemple in tweet
+        const cleanedTweet = rawTweet.replace(/(\s+|^)@\S+/g, "");
+
+        // remove url in tweet
+        return cleanedTweet.replace(/https?:\/\/.*?[\s+]/g, "");
+    }
+
     async getLastTweetByUsername(username) {
 
         const readOnlyClient = this.twitterClient.readOnly;
 
         const user = await readOnlyClient.v2.userByUsername(username);
 
-        const tweet = await readOnlyClient.v2.userTimeline(user.data.id, {max_results: 5});
-        return tweet.tweets[0].text;
+        const tweetList = await readOnlyClient.v2.userTimeline(user.data.id, {max_results: 5});
+
+        const tweet = tweetList.tweets[0].text;
+
+        return this.cleanTweet(tweet);
     }
 }
