bdd3461f755ba5fcfd654d024ce97005b81e6b6a
Julien FAURE <julien.faure@mines-ales.org>
2022-09-13 14:25:19 +0200

    Backend test updated

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-import {getLastTweetByUsername} from '../src/js/app.js';
+import TwitterService from '../server/services/TwitterService.js';
 import * as assert from 'assert';
 
 describe('Array', function () {
     describe('#getLastTweetByUsername()', function () {
         it('should return last tweet given a username', function () {
-            getLastTweetByUsername("RGopigo").then(function (tweet) {
+            new TwitterService().getLastTweetByUsername("RGopigo").then(function (tweet) {
                 assert.equal(tweet, "test fct tweeter");
             });
 
