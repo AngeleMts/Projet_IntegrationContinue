f9419514458188a6562aad2eed09d4a077527157
Luna <lunaloiselramel@gmail.com>
2022-09-13 14:36:47 +0200

    getAnalysis return data object

--- src/js/apps/HomePageApp.js
+++ src/js/apps/HomePageApp.js
@@ -28,12 +28,23 @@ function sliderTick() {
 searchButton.addEventListener('click', () => {
     new TwitterService().getLastTweetByUsername(searchTextBox.value)
         .then(value => {
+            console.log("VALUE : "+value);
             const parsedValue = JSON.parse(value);
             console.log(parsedValue);
             console.log("ID : " + parsedValue.id)
             console.log("TWEET : " + parsedValue.tweet);
             new SentimentAnalysisService().getAnalysis(parsedValue.tweet).then(result => {
-                console.log("RESULTAT : " + result);
+                const parsedAnalysis = JSON.parse(result); 
+                console.log(parsedAnalysis);
+                const msg = `Your text sounds ${parsedAnalysis.sentiment
+                }. It has ${Math.floor(
+                    parsedAnalysis.aggregate_sentiment.pos * 100
+                )}% positivity, and ${Math.floor(
+                    parsedAnalysis.aggregate_sentiment.neg * 100
+                )}% negativity. It has a neutral level of ${Math.floor(
+                    parsedAnalysis.aggregate_sentiment.neu * 100
+                )}%.`;
+                console.log("ANALYSIS : "+msg);
             }).catch(e => {
                 console.error(e);
             })
